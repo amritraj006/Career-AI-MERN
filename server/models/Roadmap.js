707d0e8ee4747f9129a5af5d5512de5e2af22413
Amrit Raj <150701418+amritraj006@users.noreply.github.com>
2025-08-29 09:02:40 +0530

    Update Roadmap.js

--- server/models/Roadmap.js
+++ server/models/Roadmap.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const roadmapSchema = new mongoose.Schema({
-  user_email: { type: String },
-  prompt: { type: String },
-  roadmap: { type: String },
+  user_email: { type: String, required: true },
+  prompt: { type: String, required: true },
+  roadmap: { type: String, required: true },
   created_at: { type: Date, default: Date.now }
 });
 
